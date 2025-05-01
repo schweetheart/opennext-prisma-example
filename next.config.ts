789e466d512b7ed2b98ce15ca928399ab01085f2
Liam Schuitemaker <liamschuitemaker@Liams-MBP-2.localdomain>
2025-05-01 11:10:50 -0700

    Add serverExternalPackages & PR593 pre-release

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["@prisma/adapter-pg", "pg", "@prisma/client"],
 };
 
 export default nextConfig;
 
 // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 initOpenNextCloudflareForDev();
